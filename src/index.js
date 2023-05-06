201b52be80c44dcf9d3ce2b639b76c7e193a3af4
Denio Nhanale <dnhanale@gmail.com>
2023-05-06 13:50:27 +0200

    Adding the Sidebar Toggle Interaction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+// Sidebar toggle function
+const sidebarToggle = () => {
+  document.body.classList.toggle('sidebar-open');
+};
+
+// Sidebar trigger
+const sidebarTrigger = document.getElementById('main-header__sidebar-toggle');
+
+// Add the event listener
+sidebarTrigger.addEventListener('click', sidebarToggle);
