f6a490f49fbeb2b855aa0f0fc27b41f6f033583d
Denio Nhanale <dnhanale@gmail.com>
2023-05-06 16:11:50 +0200

    Adding the Dark/Light Toggle

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@ const sidebarTrigger = document.getElementById('main-header__sidebar-toggle');
 
 // Add the event listener
 sidebarTrigger.addEventListener('click', sidebarToggle);
+
+//Theme switcher function
+const switchTheme = () => {
+    //Get root element and data-theme value
+    const rootElem = document.documentElement
+let dataTheme = rootElem.getAttribute('data-theme'),
+newTheme
+
+newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+//Set the new HTML attribute
+rootElem.setAttribute('data-theme', newTheme)
+
+//Set the new localStorage item
+localStorage.setItem('theme', newTheme)
+
+}
+
+
+//Add thee event listener for the theme switcher
+document.querySelector('#sidebar__theme-switcher') .
+addEventListener('click', switchTheme)
